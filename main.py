90fe947e8001b7bd5156b021cf73d870044135c6
Bhargav Chataut <bhargavchataut101@gmail.com>
2025-03-24 01:01:24 -0500

    Add feature to access Webcam and show case it

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import cv2
-import mediapipe as mp
 
-# Intialize Mediapipe Hands
-mp_hands = mp.solutions.hands
-mp_draw = mp.solutions.drawing_utils
-
-# Open webcam
+# Access webcam
 cap = cv2.VideoCapture(0)
+
+while True:
+    success, frame = cap.read()
+    if not success:
+        break
+    
+    # Show the iamge in the frame
+    cv2.imshow("Webcam", frame)
+
+    # Close the application when q is pressed
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+# To free up resourcesnused by cv2 applications
+cap.release()
+cv2.destroyAllWindows()
