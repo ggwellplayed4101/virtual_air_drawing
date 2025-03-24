8cfedecadeff260dc527fcd26e89ab29f168d5d5
Bhargav Chataut <bhargavchataut101@gmail.com>
2025-03-24 01:31:36 -0500

    feat: add detection of hand and skeletons appearing in them

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import mediapipe as mp
 
 # Access MediaPipe's hand tracking module and visulaization module
 mp_hands = mp.solutions.hands
-mp_draw = mp.solution.drawing_utils
+mp_draw = mp.solutions.drawing_utils
 
 # Create a instance of hand object
 hands = mp_hands.Hands()
@@ -12,15 +12,23 @@ hands = mp_hands.Hands()
 # Access webcam
 cap = cv2.VideoCapture(0)
 
+# Load a random image of a cat (with no hands)
+cat_frame = cv2.imread('cat_image.jpeg')
+
 while True:
     success, frame = cap.read()
     if not success:
         break
     
-    # Show the iamge in the frame
-    cv2.imshow("Webcam", frame)
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    results =  hands.process(frame_rgb)
 
-    # Close the application when q is pressed
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            mp_draw.draw_landmarks(frame, hand_landmarks,
+                                   mp_hands.HAND_CONNECTIONS)
+
+    cv2.imshow("Webcam", frame)
     if cv2.waitKey(1) == ord('q'):
         break
 
